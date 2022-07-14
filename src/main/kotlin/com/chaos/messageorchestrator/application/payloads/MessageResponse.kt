71850146f754295fe936f55eab0136f3a78b9568
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-07-14 01:38:56 -0300

    feat: add a constructor

--- src/main/kotlin/com/chaos/messageorchestrator/application/payloads/MessageResponse.kt
+++ src/main/kotlin/com/chaos/messageorchestrator/application/payloads/MessageResponse.kt
@@ -1,9 +1,15 @@
 package com.chaos.messageorchestrator.application.payloads
 
 import com.chaos.messageorchestrator.domain.entities.Destination
+import com.chaos.messageorchestrator.domain.entities.Message
 import java.time.LocalDateTime
 
 data class MessageResponse(
-    private val creationDate: LocalDateTime,
+    private val creationDate: LocalDateTime?,
     private val destination: Destination
-)
+) {
+    constructor(message: Message) : this(
+        creationDate = message.date,
+        destination = message.destination
+    )
+}
