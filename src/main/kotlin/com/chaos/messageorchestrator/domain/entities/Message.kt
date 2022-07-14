6aa4213bd66b059eb5af6f32e02504a3ff823262
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-07-14 01:30:58 -0300

    fix: set author data class

--- src/main/kotlin/com/chaos/messageorchestrator/domain/entities/Message.kt
+++ src/main/kotlin/com/chaos/messageorchestrator/domain/entities/Message.kt
@@ -2,8 +2,8 @@ package com.chaos.messageorchestrator.domain.entities
 
 import java.time.LocalDateTime
 
-data class Message(
-    private val author: String,
+open class Message(
+    private val author: Author,
     private val date: LocalDateTime? = LocalDateTime.now(),
     private val content: String,
     private val destination: Destination
